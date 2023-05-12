fab7902ee6d68e1133e086b098e1a07b7f000e61
pawelk <kumorowski.p@gmail.com>
2023-05-12 22:57:37 +0200

    Wysyłanie telemetrii

---
+++ Agent/Agent/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Azure.Devices.Client;
+using DeviceSDK;
+using Opc.UaFx.Client;
+using Opc.UaFx;
+
+string deviceConnectionString = "HostName=zajecia2023.azure-devices.net;DeviceId=Device1;SharedAccessKey=eVc1/BofDrZdvWYa8JBrBVmMyTLPmtSoxZy1WXE05MA=";
+List<string> deviceIds = new List<string>();
+
+try
+{
+    using (var client = new OpcClient("opc.tcp://localhost:4840"))
+    {
+        client.Connect();
+        Console.WriteLine("Client is connected.");
+
+        var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString);
+        await deviceClient.OpenAsync();
+        var device = new VirtualDevice(deviceClient);
+        Console.WriteLine("Połączenia udane");
+        await device.InitializeHandlers();
+        Console.WriteLine("Inicjalizacja udana");
+
+        var node = client.BrowseNode(OpcObjectTypes.ObjectsFolder);
+        SetDeviceIds(node, deviceIds);
+
+        while(true)
+        {
+            await SendTelemetry(client, deviceIds[0], device);
+            await Task.Delay(1000);
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+async Task SendTelemetry(OpcClient client, string deviceId, VirtualDevice device)
+{
+    var telemetry = new
+    {
+        production_status = client.ReadNode($"{deviceId}/ProductionStatus").Value,
+        workorder_id = client.ReadNode($"{deviceId}/WorkorderId").Value,
+        goodcount = client.ReadNode($"{deviceId}/GoodCount").Value,
+        badcount = client.ReadNode($"{deviceId}/BadCount").Value,
+        temperature = client.ReadNode($"{deviceId}/Temperature").Value
+    };
+
+    await device.SendMessage(telemetry);
+}
+
+void SetDeviceIds(OpcNodeInfo node, List<string> deviceIds, int level = 0)
+{
+    if (level == 1 && node.NodeId.ToString().Contains("Device"))
+        deviceIds.Add(node.NodeId.ToString());
+    level++;
+    foreach (var child in node.Children())
+        SetDeviceIds(child, deviceIds, level);
+}
---
+++ Agent/DeviceSDK/VirtualDevice.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+using System.Net.Mime;
+using System.Text;
+namespace DeviceSDK
+{
+    public class VirtualDevice
+    {
+        private readonly DeviceClient deviceClient;
+
+        public VirtualDevice(DeviceClient deviceClient)
+        {
+            this.deviceClient = deviceClient;
+        }
+
+        #region Sending Messages
+        public async Task SendMessage(object data)
+        {
+            var dataString = JsonConvert.SerializeObject(data);
+            Message eventMessage = new Message(Encoding.UTF8.GetBytes(dataString));
+            eventMessage.ContentType = MediaTypeNames.Application.Json;
+            eventMessage.ContentEncoding = "utf-8";
+            await deviceClient.SendEventAsync(eventMessage);
+        }
+        #endregion
+
+        #region Receiving Messages
+        private async Task OnC2dMessageReceivedAsync(Message receivedMessage, object _)
+        {
+            Console.WriteLine($"\t{DateTime.Now}> C2D message callback - message received with Id={receivedMessage.MessageId}");
+            PrintMessages(receivedMessage);
+            await deviceClient.CompleteAsync(receivedMessage);
+            Console.WriteLine($"\t{DateTime.Now}> Completed C2D message with Id = {receivedMessage.MessageId}.");
+
+            receivedMessage.Dispose();
+        }
+
+        private void PrintMessages(Message receivedMessage)
+        {
+            string messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+            Console.WriteLine($"\t\tReceived message: {messageData}");
+
+            int propCount = 0;
+            foreach (var prop in receivedMessage.Properties)
+            {
+                Console.WriteLine($"\t\tProperty[{propCount++}]> Key={prop.Key} : Value={prop.Value}");
+            }
+        }
+        #endregion
+
+        #region Direct methods
+
+        private async Task<MethodResponse> DefaultServiceHandler(MethodRequest methodRequest, object userContext)
+        {
+            Console.WriteLine($"\tMETHOD EXECUTED: {methodRequest.Name}");
+
+            await Task.Delay(1000);
+
+            return new MethodResponse(0);
+        }
+        #endregion
+
+        #region Device Twin
+        public async Task UpdateTwinAsync()
+        {
+            var twin = await deviceClient.GetTwinAsync();
+
+            Console.WriteLine($"\n Initial twin value received: \n{JsonConvert.SerializeObject(twin, Formatting.Indented)}");
+            Console.WriteLine();
+
+            var reportedProperties = new TwinCollection();
+            reportedProperties["DateTimeLastAppLaunch"] = DateTime.Now;
+
+            await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
+        }
+
+        private async Task OnDesiredPropertyChanged(TwinCollection desiredProperties, object _)
+        {
+            Console.WriteLine($"\tDesired property change\n\t{JsonConvert.SerializeObject(desiredProperties)}");
+            Console.WriteLine("\tSending current time as reported property");
+
+            TwinCollection reportedProperties = new TwinCollection();
+            reportedProperties["DateTimeLastDesiredPropertyChangeReceived"] = DateTime.Now;
+
+            await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
+        }
+        #endregion
+        public async Task InitializeHandlers()
+        {
+            await deviceClient.SetReceiveMessageHandlerAsync(OnC2dMessageReceivedAsync, deviceClient);
+            await deviceClient.SetMethodDefaultHandlerAsync(DefaultServiceHandler, deviceClient);
+            await deviceClient.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertyChanged, deviceClient);
+        }
+
+    }
+}
